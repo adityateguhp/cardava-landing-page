e5205bb5819a0ee6af7e2e66a5d2be0f1108c78a
Aditya Teguh Pratama <pratamateguhaditya@gmail.com>
2021-01-25 23:39:57 +0700

    tes

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const App = () => (
   </BrowserRouter>
 );
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
